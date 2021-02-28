c8f9fe113a506c0b3f96eacae25b6e997db5e4b1
kate-krasnoselska <kate.krasnoselska@gmail.com>
2021-02-28 09:16:09 +0200

    Add test

--- Calculator.Tests/Calculator.Tests/CalculatorTests.cs
+++ Calculator.Tests/Calculator.Tests/CalculatorTests.cs
@@ -2,12 +2,14 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace Calculator.Tests
 {
 	public class CalculatorTests
 	{
         IWebDriver browser;
+        private object driver;
 
         [SetUp]
         public void BeforeEachTest()
@@ -77,6 +79,36 @@ namespace Calculator.Tests
             browser.FindElement(By.Id("amount")).SendKeys("100");
             browser.FindElement(By.Id("percent")).SendKeys(" ");
             browser.FindElement(By.Id("term")).SendKeys("365");
+            browser.FindElement(By.Id("d365")).Click();
+            string actualIncome = browser.FindElement(By.Id("income")).GetAttribute("value");
+            Assert.AreEqual("100.00", actualIncome);
+            string actualInterest = browser.FindElement(By.Id("interest")).GetAttribute("value");
+            Assert.AreEqual("0.00", actualInterest);
+
+        }
+
+        [Test]
+        public void PositiveTestInvestmentTermIsMandatoryField()
+        {
+            browser.FindElement(By.Id("amount")).SendKeys("100");
+            browser.FindElement(By.Id("percent")).SendKeys("10");
+            browser.FindElement(By.Id("term")).SendKeys(" ");
+            browser.FindElement(By.Id("d365")).Click();
+            string actualIncome = browser.FindElement(By.Id("income")).GetAttribute("value");
+            Assert.AreEqual("100.00", actualIncome);
+            string actualInterest = browser.FindElement(By.Id("interest")).GetAttribute("value");
+            Assert.AreEqual("0.00", actualInterest);
+
+        }
+
+        [Test]
+        public void PositiveTestStartDateIsMandatoryField()
+        {
+            browser.FindElement(By.Id("amount")).SendKeys("100");
+            browser.FindElement(By.Id("percent")).SendKeys("10");
+            browser.FindElement(By.Id("term")).SendKeys("365");
+            SelectElement selectElement = new SelectElement((IWebElement)By.Id("day"));
+            selectElement.SelectByValue("28");
             browser.FindElement(By.Id("d365")).Click();
             string actualIncome = browser.FindElement(By.Id("income")).GetAttribute("value");
             Assert.AreEqual("100.00", actualIncome);
