a4d4f5acf325209e6f994b455b337b67ab6171e0
kate-krasnoselska <kate.krasnoselska@gmail.com>
2021-04-12 19:56:01 +0300

    Added work progress
    Settings page. But code does not work

--- Calculator.Tests/Calculator.Tests/SettingsPageTests.cs
+++ Calculator.Tests/Calculator.Tests/SettingsPageTests.cs
@@ -75,7 +75,7 @@ namespace Calculator.Tests
             // 1. Select the Date format "MM dd yyyy" from dropdown
             // 2. Click Save
             // 3. Click OK alert
-            // 4. See End date is aaplied correctly
+            // 4. See End date is applied correctly
 
             SelectElement dateFormatSelect = new SelectElement(element: browser.FindElement(By.XPath("//select[@id = 'dateFormat']")));
             dateFormatSelect.SelectByText(format);
@@ -87,5 +87,55 @@ namespace Calculator.Tests
 
             Assert.AreEqual(expected.ToString (format, CultureInfo.InvariantCulture), actual);
         }
+
+        [TestCase("123,456,789.00")]
+        [TestCase("123.456.789,00")]
+        [TestCase("123 456 789.00")]
+        [TestCase("123 456 789,00")]
+        public void SelectNumberFormatIsApplied(string format)
+        {
+            // 1. Select the Number format
+            // 2. Click Save
+            // 3. Click OK alert
+            // 4. Check if selected number format = income field format
+            // 5. check if selected number format = interest earned field format
+
+            SelectElement numberFormatSelect = new SelectElement(element: browser.FindElement(By.XPath("//select[@id = 'numberFormat']")));
+            numberFormatSelect.SelectByText(format);
+            browser.FindElement(By.XPath("//button[text()='Save']")).Click();
+            browser.SwitchTo().Alert().Accept();
+
+            browser.FindElement(By.XPath("//input [@id = 'income']")).GetAttribute("value");
+            // browser.FindElement(By.XPath("//input [@id = 'interest']")).GetAttribute("value");
+
+            string actual = browser.FindElement(By.XPath("//input [@id = 'income']")).GetAttribute("value");
+
+            Assert.AreEqual(format, actual);
+            //Assert.AreEqual(format, );
+
+            // NEED HELP: how to add second assert? it could not be named actual too, right?
+        }
+
+        [TestCase("$ - US dollar")]
+        [TestCase("€")]
+        [TestCase("£")]
+        public void SelectCurrencyFormatIsApplied(string format)
+
+        {
+            // 1. Select the Currency format
+            // 2. Click Save
+            // 3. Click OK alert
+            // 4. Check if selected currency format is applied on Calculator page
+
+            SelectElement currencyFormatSelect = new SelectElement(element: browser.FindElement(By.XPath("//select[@id = 'currency']")));
+            currencyFormatSelect.SelectByText(format);
+            browser.FindElement(By.XPath("//button[text()='Save']")).Click();
+            browser.SwitchTo().Alert().Accept();
+            browser.FindElement(By.XPath("//td[@id = 'currency']")).GetAttribute("textContent");
+            string actual = browser.FindElement(By.XPath("//td[@id = 'currency']")).GetAttribute("textContent");
+
+            Assert.AreEqual(format, actual);
+        } 
+
     }
 }
