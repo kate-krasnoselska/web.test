72f5a29791a3efc2261bac20d0ba188d400aff32
kate-krasnoselska <kate.krasnoselska@gmail.com>
2021-05-30 14:36:52 +0300

    rewrote to a new branch

---
+++ Calculator.Tests/Calculator.Tests/HistoryPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Calculator.Tests.Pages
+{
+    public class HistoryPage
+    {
+        private IWebDriver driver;
+        public HistoryPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement CalculatorBtn => driver.FindElement(By.XPath("//button[text()='Calculator']"));
+
+        public IWebElement ClearBtn => driver.FindElement(By.XPath("//button[@id = 'clear']"));
+
+        //public IWebElement CalculatedAmount => driver.FindElement(By.XPath(".//td[1]"));
+
+        //public IWebElement CalculatedData => driver.FindElement(By.XPath(".//tr[@class = 'data']"));
+
+        //public string CalculatedDataCleared => driver.FindElement(By.ClassName("data")).GetAttribute("value");
+    }
+
+}
---
+++ Calculator.Tests/Calculator.Tests/HistoryPageTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Calculator.Tests.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Calculator.Tests
+{
+    public class HistoryPageTests
+    {
+        IWebDriver browser;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            browser = new ChromeDriver();
+            browser.Url = "http://127.0.0.1:8080";
+            browser.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            new LoginPage(browser).Login();
+
+            browser.FindElement(By.XPath("//td[2]//input[@id = 'amount']")).SendKeys("100");
+            browser.FindElement(By.XPath("//input [@id = 'percent']")).SendKeys("10");
+            browser.FindElement(By.XPath("//input [@id = 'term']")).SendKeys("365");
+            browser.FindElement(By.XPath("//input[@type][2]")).Click();
+
+            new CalculatorPage(browser).Calculate();
+
+            browser.FindElement(By.XPath("//button[text()='History']")).Click();
+        }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+            browser.Quit();
+        }
+
+        [Test]
+        public void CalculatorButtonWork()
+        {
+            HistoryPage historyPage = new HistoryPage(browser);
+            historyPage.CalculatorBtn.Click();
+
+            string actual = browser.Url;
+
+            Assert.AreEqual("http://127.0.0.1:8080/Deposit", actual);
+        }
+
+        [Test]
+        public void ClearBtnWork()
+        {
+            HistoryPage historyPage = new HistoryPage(browser);
+            historyPage.ClearBtn.Click();
+            // NEED HELP: Which assert to use here? I want to say that data is cleared
+            //Assert.AreEqual(historyPage.CalculatedAmount, " ");
+        }
+
+
+    }
+}
+
+
