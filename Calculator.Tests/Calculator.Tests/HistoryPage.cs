996823cb352583208a3fe3c72a2dfe273f96b74e
kate-krasnoselska <kate.krasnoselska@gmail.com>
2021-05-30 09:36:56 +0300

    Added History

---
+++ Calculator.Tests/Calculator.Tests/HistoryPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Calculator.Tests.Pages
+{
+    public class HistoryPage
+    {
+        private IWebDriver driver;
+        public HistoryPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+    }
+}
---
+++ Calculator.Tests/Calculator.Tests/HistoryPageTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Calculator.Tests.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Calculator.Tests
+{
+    public class HistoryPageTests
+    {
+        IWebDriver browser;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            browser = new ChromeDriver();
+            browser.Url = "http://127.0.0.1:8080";
+            browser.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(60);
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            new LoginPage(browser).Login();
+
+            browser.FindElement(By.XPath("//button[text()='History']")).Click();
+        }
+
+        [TearDown]
+        public void AfterEachTest()
+        {
+            browser.Quit();
+        }
+    }
+}
