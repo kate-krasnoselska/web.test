f7249e669b72a0fb0046e590f52c04651ce2029d
kate-krasnoselska <kate.krasnoselska@gmail.com>
2021-06-21 14:27:20 +0300

    1

--- Calculator.Tests/Calculator.Tests/HistoryPage.cs
+++ Calculator.Tests/Calculator.Tests/HistoryPage.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using OpenQA.Selenium;
 
 namespace Calculator.Tests.Pages
@@ -19,6 +21,21 @@ namespace Calculator.Tests.Pages
         public IWebElement HeaderAmount => driver.FindElement(By.XPath("//tr[@class = 'header']//th[text() = 'Amount']"));
 
         public IWebElement CalculatedData => driver.FindElement(By.XPath("//tr[@class = 'data']//td[text() = '100']"));
+
+        //many
+        public int RowCount => driver.FindElements(By.XPath("//tr[@class = 'data']")).Count;
+
+        //select specific row
+        public List<string> GetRowData(int rowNumber)
+        {
+            List<string> result = new List<string>();
+            List<IWebElement> cells = driver.FindElements(By.XPath("//tr[@class = 'data']["+ rowNumber +"]//td")).ToList();
+            foreach (IWebElement cell in cells)
+            {
+                result.Add(cell.Text);
+            }     
+            return result;
+        }
     }
 
 }
--- Calculator.Tests/Calculator.Tests/Pages/SettingPage.cs
+++ Calculator.Tests/Calculator.Tests/Pages/SettingPage.cs
@@ -61,4 +61,5 @@ namespace Calculator.Tests.Pages
             Save();
         }
     }
+
 }
