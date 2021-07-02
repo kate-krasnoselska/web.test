5bb3ed9821d2b7e0094e4e8e898a9047560ed564
kate-krasnoselska <kate.krasnoselska@gmail.com>
2021-07-02 09:47:01 +0300

    new tests

---
+++ Calculator.Tests/Calculator.Tests/Features/SettingsPage.feature
@@ -0,0 +1,7 @@
+﻿Feature: Settings 
+
+Scenario: Logout
+	Given I am on Settings page
+	When I click Logout button
+	Then I am redirected to Login Page
+     dbfdmfb 
---
+++ Calculator.Tests/Calculator.Tests/Features/SettingsPageSteps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using TechTalk.SpecFlow;
+
+namespace Calculator.Tests.Features
+{
+    [Binding]
+    public class SettingsPageSteps
+    {
+        [Given("I am on Settings page")]
+        public void OpenSettingsPage()
+        {
+            
+        }
+      
+    }
+}
